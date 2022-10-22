3df481edb2d16644c0ea5a4db14ec18e8d7dee1b
Sterling Sutton <sterlingsutton2006@gmail.com>
2022-10-22 12:41:21 -0700

    Make it easier to change GOL rules.

--- src/main/java/github/sterlingsutton2006/Grid.java
+++ src/main/java/github/sterlingsutton2006/Grid.java
@@ -4,26 +4,28 @@ import java.util.Random;
 
 public class Grid {
     // Attributes
-    public final int X_LENGTH = 10;
-    public final int Y_LENGTH = 10;
-    private Cell[][] grid = new Cell[X_LENGTH][Y_LENGTH];
+    public final int X_LENGTH;
+    public final int Y_LENGTH;
+    private Cell[][] grid;
 
     // Methods
-    public Grid() {
-        for (int x = 0; x < X_LENGTH; x++) {
-            for (int y = 0; y < Y_LENGTH; y++) {
+    private boolean decideState(int numAliveNeighbors, boolean isAlive) {
+        if (isAlive) {
+            return (numAliveNeighbors == 2 || numAliveNeighbors == 3);
+        }
+        else {
+            return (numAliveNeighbors == 3);
+        }
+    }
+    public Grid(int X_LENGTH, int Y_LENGTH) {
+        this.X_LENGTH = X_LENGTH;
+        this.Y_LENGTH = Y_LENGTH;
+        grid = new Cell[this.X_LENGTH][this.Y_LENGTH];
+        for (int x = 0; x < this.X_LENGTH; x++) {
+            for (int y = 0; y < this.Y_LENGTH; y++) {
                 grid[x][y] = new Cell();
             }
         }
-    }
-
-    public void spawnGlider(int coordX, int coordY) {
-        Cell[][] nextGrid = Utility.copyGrid(grid);
-        nextGrid[coordX][coordY].setAlive(true);
-        nextGrid[1 + coordX][coordY].setAlive(true);
-        nextGrid[2 + coordX][coordY].setAlive(true);
-        nextGrid[2 + coordX][1 + coordY].setAlive(true);
-
     }
 
     public void populateGrid() {
@@ -58,18 +60,25 @@ public class Grid {
                             case 6 -> currentCellState = grid[x][y - 1].getAlive();
                             case 7 -> currentCellState = grid[x + 1][y - 1].getAlive();
                         }
-                        if (currentCellState) {
-                            numAliveNeighbors++;
-                        }
                     } catch (Exception ignored) {
+                        switch (i) {
+                            case 0 -> currentCellState = grid[0][y].getAlive();
+                            case 1 -> currentCellState = grid[0][0].getAlive();
+                            case 2 -> currentCellState = grid[x][0].getAlive();
+                            case 3 -> currentCellState = grid[X_LENGTH - 1][0].getAlive();
+                            case 4 -> currentCellState = grid[X_LENGTH - 1][y].getAlive();
+                            case 5 -> currentCellState = grid[X_LENGTH - 1][Y_LENGTH - 1].getAlive();
+                            case 6 -> currentCellState = grid[x][Y_LENGTH - 1].getAlive();
+                            case 7 -> currentCellState = grid[0][Y_LENGTH - 1].getAlive();
+                        }
+                    }
+                    if (currentCellState) {
+                        numAliveNeighbors++;
                     }
                 }
+                nextGrid[x][y].setAlive(decideState(numAliveNeighbors, nextGrid[x][y].getAlive()));
 
-                if (numAliveNeighbors == 3 && !grid[x][y].getAlive()) {
-                    nextGrid[x][y].setAlive(true);
-                } else if (!(numAliveNeighbors == 2 || numAliveNeighbors == 3) && grid[x][y].getAlive()) {
-                    nextGrid[x][y].setAlive(false);
-                }
+
             }
         }
 
@@ -77,15 +86,8 @@ public class Grid {
     }
 
     public void printGrid() {
-        System.out.println();
-        System.out.println();
-        for (int i = 0; i < X_LENGTH; i++) {
-            System.out.print("****");
-        }
-        System.out.println();
-
         try {
-            Thread.sleep(1000);
+            Thread.sleep(20);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -94,14 +96,11 @@ public class Grid {
             System.out.println();
             for (int y = 0; y < Y_LENGTH; y++) {
                 if (grid[x][y].getAlive()) {
-                    System.out.print("#");
+                    System.out.print("██");
                 } else {
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
 
-                if (y < Y_LENGTH - 1) {
-                    System.out.print(" | ");
-                }
             }
         }
     }
--- src/main/java/github/sterlingsutton2006/Main.java
+++ src/main/java/github/sterlingsutton2006/Main.java
@@ -3,7 +3,7 @@ package github.sterlingsutton2006;
 public class Main {
 
     public static void main(String[] args) {
-        Grid grid = new Grid();
+        Grid grid = new Grid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         grid.populateGrid();
         for (int i = 0; i < 10000; i++) {
             grid.nextGeneration();
